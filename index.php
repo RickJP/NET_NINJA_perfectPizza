329d6672dfe32769b7acedf7f3aa66df04c51534
Rick <3741326+RickJP@users.noreply.github.com>
2019-03-18 09:34:40 +0900

    very late initial commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php 
+
+
+  // connect to db
+  $conn = mysqli_connect('localhost', 'rick', 'test123', 'PerfectPizza');
+  // check the connection
+
+  if (!$conn) {
+    echo 'Connection Error! ' . mysqli_connect_error();
+  }
+
+  // query for all pizzas
+
+  $sql = 'SELECT id, title, ingredients FROM pizzas ORDER BY created_at';
+
+  // make query & get result
+  $result = mysqli_query($conn, $sql);
+
+  // fetch resulting rows as an associative array
+  $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+  // free result from memory
+  mysqli_free_result($result);
+
+  mysqli_close($conn);
+
+  //explode(',', $pizzas[0]['ingredients']);
+
+  
+?>
+
+<!DOCTYPE html>
+<html>
+	
+	<?php include('templates/header.php'); ?>
+
+  <h4 class="center grey-text">Pizzas!</h4>
+
+  <div class="container">
+    <div class="row">
+      <?php  foreach($pizzas as $pizza): ?>
+      
+        <div class="col s6 md3">
+          <div class="card z-depath-0">
+            <div class="card-content center">
+              <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+              <ul>
+                <?php foreach(explode(',', $pizza['ingredients']) as $ingred): ?>
+                  <li><?php echo htmlspecialchars($ingred); ?></li>
+                <?php endforeach;  ?>
+              </ul>
+            </div>
+            <div class="card-action right-align">
+              <a href="#" class="brand-text">more info</a>
+            </div>
+          </div>
+        </div>
+      
+      <?php endforeach;  ?>
+
+      <!-- <?php if(count($pizzas) >= 5):  ?>
+        <p>There are two or more pizzas.</p>
+      <?php  else: ?>
+        <p>There are less than 5 pizzas.</p>
+      <?php endif; ?> -->
+    </div>
+  </div>
+
+	<?php include('templates/footer.php'); ?>
+
+</html>
